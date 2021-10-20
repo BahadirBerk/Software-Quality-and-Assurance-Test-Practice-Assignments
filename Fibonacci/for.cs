652af201c7c6a44deb55bd2fe49ccedc9081850f
Bahadır Berk <browntoclown@gmail.com>
2021-10-20 04:24:14 +0300

    last edit

--- Fibonacci/for.cs
+++ Fibonacci/for.cs
@@ -1,12 +1,23 @@
-public static voidFibonacci_Iterative(int len)  
-{  
-    int a = 0, b = 1, c = 0;  
-    Console.Write("{0} {1}", a,b);  
-    for (int i = 2; i < len; i++)  
-    {  
-        c= a + b;  
-        Console.Write(" {0}", c);  
-        a= b;  
-        b= c;  
-    }  
-}  
+using System.Diagnostics;
+using System;  
+  public class FibonacciExample  
+   {  
+     public static void Main(string[] args)  
+      {  
+         Stopwatch stopwatch = new Stopwatch();
+         stopwatch.Start();
+         int n1=0,n2=1,n3,i,number;    
+         Console.Write("Kaça kadar fibonacci sayısı gösterilsin? : ");    
+         number = int.Parse(Console.ReadLine());  
+         Console.Write(n1+" "+n2+" "); 
+         for(i=2;i<number;++i)  
+         {    
+          n3=n1+n2;    
+          Console.Write(n3+" ");    
+          n1=n2;    
+          n2=n3;    
+         }
+         stopwatch.Stop();
+         Console.WriteLine("Geçen Süre:  "+stopwatch.Elapsed);
+      }  
+   }  
